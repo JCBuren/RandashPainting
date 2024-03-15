3079fb79e47bc9d10d715f412b2b81d3bd5c8bbd
Jordan <JBuren1957@Gmail.com>
2024-03-14 23:01:27 -0500

    Graphics added

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -3,6 +3,7 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Image from "next/image"
 
 export default function Team() {
 	return (
@@ -10,62 +11,47 @@ export default function Team() {
 			<div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-[url('../app/assets/Fitness.jpg')]">
 				<div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[2]" />
 				<div className="p-5 text-white z-[2] mt-[-10rem]">
-					<h2 className="text-5xl mt-20 font-bold">About Us</h2>
-					<p className="py-5 text-2xl">
-						Three Generations of Painting
-					</p>
-					
+					<h2 className="text-5xl mt-20 font-bold">
+						About Randash Painting
+					</h2>
+					<p className="py-5 text-2xl">Three Generations of Painting</p>
 				</div>
 			</div>
-			<div className="flex items-center justify-center mb-12 bg-fixed bg-center bg-cover bg-cyan-900/50 ">
-				<div className="p-5 text-white">
-					<h1 className="text-4xl mt-40 pb-10">About Randash Painting</h1>
-					<div className="grid grid-cols-2 gap-8">
-						<div className="lg:text-xl">
-							Randash Painting LLC is owned and operated by Adam Randash.
-							Adam is a third generation painting contractor. He has been
-							working with the family business since 2005. Randash
-							Painting has a reputation for unmatched quality throughout
-							the Fargo/ Moorhead area and beyond. Adam has excellent
-							attention to detail and has perfected his painting skills
-							over the years. He is thrilled to take on complex and
-							challenging projects. Randash Painting has a small talented
-							work crew that will be tedious, courteous, and thorough
-							while completing your project.
-						</div>
-						<img
-							src="https://i.imgur.com/qwr6q9j.jpg"
-							className="max-w-[75%] max-h-[75%] ml-10"
-						/>
-					</div>
-					<div className="grid lg:grid-cols-3 auto-cols-[300px] gap-6 items-center ">
-						<div className="font-bold border-2 rounded-xl p-4">
-							How we started: The Randash Painting business was
-							originated by Adam’s grandfather, Elmer Randash in 1950.
-							Elmer was part of the SEABEES in the Navy. While enlisted
-							during WW2 he served as an aircraft painter in California.
-							He brought his painting skills home after the war concluded
-							to start the business in the FM area.
-						</div>
-
-						<div className="font-bold border-2 rounded-xl p-4">
-							{" "}
-							Through the years: In 1985 Dave Randash, Elmer’s son took
-							over the business. Dave Randash built up a reputation of
-							superior craftsmanship for Randash Painting throughout the
-							FM area with years of service. He worked with many big
-							builders throughout the area over the years.
-						</div>
-						<div className="font-bold border-2 rounded-xl p-4">
-							Dave has now retired, and Adam is proud to be the third
-							generation to operate the family business. Adam is honored
-							to have learned his skills from his dad and to have a
-							business with the highest reputation for quality. Adam
-							started his own business after Dave retired in 2021. He has
-							been painting in the FM area since 2005 full time.
-						</div>
-					</div>
+			<div className="grid grid-flow-row justify-items-center gap-12 bg-[#F2F3F1]">
+				<Image
+					src="https://i.imgur.com/DPvQ2ZA.png"
+					alt="/"
+					width={980}
+					height={620}
+					className="rounded-xl flex"
+				/>
+				<div className="grid grid-cols-1 max-w-[50%] gap-6">
+				<h1 className="text-xl">
+					My name is Adam Randash. I am a painting contractor and owner of
+					Randash Painting, LLC. I service the Fargo/Moorhead and
+					surrounding area, with further travel upon request.{" "}
+				</h1>
+				<h1 className="text-xl">
+					My passion as a painting contractor is to make a home beautiful!
+					With all the different quality touches I can provide, I am
+					thorough and pay high attention to detail. My work ethic and
+					communication skills will help get your projects done to
+					perfection. Over the years I have worked with many high end
+					contractors in the area on new homes. I also love to work with
+					families on projects in their current homes or businesses.
+					Contact me with your ideas and I will help create stunning
+					results in your home or business. I am licensed, insured and
+					would be happy to hear from you to line up a quote on your next
+					project.
+				</h1>
 				</div>
+				<Image
+					src="https://i.imgur.com/EC2hZbp.png"
+					alt="/"
+					width={1720}
+					height={980}
+					className="ml-12"
+				/>
 			</div>
 			<Navbar />
 			<Footer />
